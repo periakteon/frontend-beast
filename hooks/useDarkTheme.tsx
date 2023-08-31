f844f6ba579e66a4204722d5aa127ff4ea944b1f
Masum Gökyüz <masumgokyuz@gmail.com>
2023-08-31 21:07:29 +0300

    hooks done

--- hooks/useDarkTheme.tsx
+++ hooks/useDarkTheme.tsx
@@ -1,15 +1,17 @@
-import { useTheme } from "nextra-theme-docs/.";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 const useDark = () => {
   const { resolvedTheme } = useTheme();
   const [isDark, setIsDark] = useState(false);
+
   useEffect(() => {
     setIsDark(resolvedTheme === "dark");
     return () => {
       false;
     };
   }, [resolvedTheme]);
+
   return isDark;
 };
 
