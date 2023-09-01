a5adebb94f69351536e73b994074fe1c1adcd033
Masum Gökyüz <masumgokyuz@gmail.com>
2023-09-01 09:15:38 +0300

    api fix

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
@@ -5,7 +6,7 @@ export const config = {
   runtime: "experimental-edge",
 };
 
-export default function handler(req: NextRequest) {
+export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
 
@@ -42,7 +43,7 @@ export default function handler(req: NextRequest) {
             <img
               alt="The Front-End Beast Logotype"
               width={300}
-              src="/images/favicon/android-chrome-512x512.png"
+              src="https://private-user-images.githubusercontent.com/121493635/264938290-89689585-bd87-479c-a0a9-154051adb08f.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE2OTM1NDg5NzgsIm5iZiI6MTY5MzU0ODY3OCwicGF0aCI6Ii8xMjE0OTM2MzUvMjY0OTM4MjkwLTg5Njg5NTg1LWJkODctNDc5Yy1hMGE5LTE1NDA1MWFkYjA4Zi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwOTAxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDkwMVQwNjExMThaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT01ZDE2M2RiYWM2ZjM0OGU3NDFkMGRkMTUwMWZjNDJlYzcyYjhlYTg2ZThlNTMzZTY1MjM2ODIzNzUwMTUyZGM1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.xre2Nhd782YJfxTNlvoi21rIQQ9KQbTIX5Ir8oF1TKc"
             />
             <p
               style={{
@@ -53,7 +54,7 @@ export default function handler(req: NextRequest) {
                 marginLeft: 20,
               }}
             >
-              Docs
+              Front-End Beast
             </p>
           </div>
           <p
