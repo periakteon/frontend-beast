2fe19ca2eecc09d1b1199b39ddd37898319fdd49
Masum Gökyüz <masumgokyuz@gmail.com>
2023-09-01 15:31:04 +0300

    google analytics install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from "react";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function Nextra({
   Component,
@@ -8,6 +9,14 @@ export default function Nextra({
 }: AppProps): ReactElement {
   return (
     <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-XFQ3VDWDJ1" />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-XFQ3VDWDJ1');`}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
